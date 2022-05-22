107cda0b24c09f5200c48235255fccf86fea4abe
sachinshelar <sachinshelar1122@gmail.com>
2022-05-22 16:44:49 +0530

    First Commit

---
+++ app/src/main/java/com/sachin/videoplayer/MainActivity.java
@@ -0,0 +1,25 @@
+package com.sachin.videoplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        VideoView videoView = findViewById(R.id.video);
+        MediaController mediaController= new MediaController(this);
+        mediaController.setAnchorView(videoView);
+        Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.sample);
+        videoView.setMediaController(mediaController);
+        videoView.setVideoURI(uri);
+        videoView.requestFocus();
+        videoView.start();
+    }
+}
